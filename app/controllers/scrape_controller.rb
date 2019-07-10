1dc5ba5f2d3be8bda89dba0b05c4c81ce27d93f0
Jacky <jaackyvan@gmail.com>
2019-07-09 22:40:56 -0700

    Parsing link to deal forum post

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 
 class ScrapeController < ApplicationController
   def index
+    root_rfd = 'https://forums.redflagdeals.com'
     doc = Nokogiri::HTML(open('https://forums.redflagdeals.com/hot-deals-f9/'))
     topictitle = doc.css('.topictitle')
 
@@ -10,10 +11,12 @@ class ScrapeController < ApplicationController
       links = tt.css('a')
       deal = {}
       if links.length == 1
-        deal[:text] = links.text
+        deal[:text] = links[0].text
+        deal[:forum_link] = root_rfd + links[0]['href']
       elsif links.length == 2
         deal[:retailer] = links[0].text
         deal[:text] = links[1].text
+        deal[:forum_link] = root_rfd + links[1]['href']
       else
         puts 'LINKS LONGER THAN 2?!'
       end
